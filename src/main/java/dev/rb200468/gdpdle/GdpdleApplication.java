04ac4cbbf0d1b668680b9cc15fb13f5cec807401
Ryan Bendall <139355026+RB200468@users.noreply.github.com>
2025-09-22 15:30:37 +0100

    initial commit

---
+++ src/main/java/dev/rb200468/gdpdle/GdpdleApplication.java
@@ -0,0 +1,13 @@
+package dev.rb200468.gdpdle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdpdleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdpdleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rb200468/gdpdle/GdpdleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rb200468.gdpdle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdpdleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
